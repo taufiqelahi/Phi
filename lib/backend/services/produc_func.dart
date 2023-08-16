be0bb99db9f456f0d711c80a02358d4834027573
taufiqelahi <taufiqelahiiceiu@gmail.com>
2023-08-16 14:32:36 +0600

    backend implementation of home screen completed

--- lib/backend/services/produc_func.dart
+++ lib/backend/services/produc_func.dart
@@ -2,10 +2,20 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:phi/backend/model/product.dart';
 
 class ProductFunc {
-  Stream<List<Product>> getAllProduct() {
-    return FirebaseFirestore.instance.collection('products').snapshots().map(
-        (event) => event.docs.map((e) => Product.fromJson(e.data())).toList());
+  Stream<List<Product>> getAllProducts({required String category}) {
+    
+    if(category.toLowerCase()=="all"){
+      return FirebaseFirestore.instance.collection('products').snapshots().map(
+              (event) => event.docs.map((e) => Product.fromJson(e.data())).toList());
+    }else{
+      return FirebaseFirestore.instance.collection('products').where("category", isEqualTo: category.toLowerCase()).snapshots().map(
+              (event) => event.docs.map((e) => Product.fromJson(e.data())).toList());
+    }
+   
   }
+  
+
+  
 
   addProduct() {}
 }
--- lib/screen/dashboard/home_screen.dart
+++ lib/screen/dashboard/home_screen.dart
@@ -1,14 +1,19 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:phi/backend/model/offer.dart';
 import 'package:phi/backend/model/product.dart';
+import 'package:phi/backend/services/offer_func.dart';
 import 'package:phi/backend/services/produc_func.dart';
 import 'package:phi/component/button.dart';
+import 'package:phi/component/custom_carousel.dart';
 import 'package:phi/component/label.dart';
+import 'package:phi/component/product_view.dart';
 import 'package:phi/utils/all_colors.dart';
 import 'package:phi/utils/font_size.dart';
 
@@ -20,14 +25,16 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<Image> imageList = [];
 
-  int current = 0;
 
-  List<String> categories = ["All", "Running", "Sneakers", "Formal", "Casual"];
-  String selectedCategory = "All";
 
-  final CarouselController controller = CarouselController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initController();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,9 +49,7 @@ class _HomeScreenState extends State<HomeScreen> {
             onPressed: () {
               Scaffold.of(context).openDrawer();
             },
-            tooltip: MaterialLocalizations
-                .of(context)
-                .openAppDrawerTooltip,
+            tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
           );
         }),
         title: Row(
@@ -65,160 +70,23 @@ class _HomeScreenState extends State<HomeScreen> {
         width: 120,
       ),
       body: Column(children: [
-        CarouselSlider(
-          carouselController: controller,
-          items: [
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: AllColors.whiteSmoke),
-              width: 400,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: Wrap(
-                  direction: Axis.vertical,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 30),
-                      child: RichText(
-                        text: TextSpan(
-                          text: '20%',
-                          style: GoogleFonts.workSans(
-                              textStyle: TextStyle(
-                                  fontSize: 40,
-                                  fontWeight: FontWeight.bold,
-                                  color: AllColors.black)),
-                          children: [
-                            TextSpan(
-                                text: ' Discount',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.normal,
-                                    fontSize: 30))
-                          ],
-                        ),
-                      ),
-                    ),
-                    const Label(
-                      text: 'on your first purchase',
-                      fonts: Fonts.workSans,
-                      fontSize: 14,
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    FillButton(
-                        title: 'Shop now',
-                        containerColor: AllColors.black,
-                        onPressed: () {}),
-                    Transform.translate(
-                        offset: Offset(-70, -15),
-                        child: Image.asset('assets/greenShoe.png'))
-                  ],
-                ),
-              ),
-            )
-          ],
-          options: CarouselOptions(
-              viewportFraction: 1,
-              height: 180,
-              onPageChanged: (index, reason) {
-                setState(() {
-                  current = index;
-                });
-              }),
-        ),
-        DotsIndicator(
-          mainAxisSize: MainAxisSize.min,
-          decorator: DotsDecorator(activeColor: AllColors.black),
-          dotsCount: 7,
-          position: current,
-        ),
-        SizedBox(
-            height: 80,
-            child: ListView.builder(
-              itemCount: categories.length,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (BuildContext context, int index) {
-                return Padding(
-                  padding: const EdgeInsets.only(right: 20),
-                  child: FillButton(
-                      pressedOpacity: 0.8,
-                      title: categories[index],
-                      textColor: selectedCategory == categories[index]
-                          ? AllColors.white
-                          : AllColors.gray61,
-                      fontSize: FontSize.p1,
-                      fontWeight: FontWeight.bold,
-                      containerColor: selectedCategory == categories[index]
-                          ? AllColors.black
-                          : Colors.transparent,
-                      onPressed: () {
-                        setState(() {
-                          selectedCategory = categories[index];
-                        });
-                      }
-                  ),
-                );
-              },
-            )),
-        StreamBuilder(stream: ProductFunc().getAllProduct(),
-            builder: (BuildContext context,
-                AsyncSnapshot<List<Product>> snapshot) {
+       FutureBuilder(
+          future: OfferFunc().getAllOffer(),
+        builder:
+            (BuildContext context, AsyncSnapshot<List<Offer>> snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
-                return CircularProgressIndicator();
+                return Container(height: 200,);
               }
-              List<Product>products=snapshot.data??[];
-              return Expanded(
-                child: GridView.count(
-                  crossAxisCount: 2,
-                  childAspectRatio: .7,
-                  children:products.map((product) =>   Container(
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    margin: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: AllColors.whiteSmoke),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          width: 150,
-                          height: 150,
-                          child: Image.network(
-                              fit:BoxFit.cover,
-                              '${product.image}'
+              List<Offer> offers = snapshot.data ?? [];
 
-                          ),
-                        ),
-                        Label(
-                          text: product.title,
-                          fontSize: FontSize.p1,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        Row(
-                          children: [
-                            Label(
-                              text: '\$${product.price}',
-                              fontSize: FontSize.p1,
-                            ),
-                            Spacer(),
-                            CupertinoButton(
-                                padding: EdgeInsets.zero,
-                                child: SvgPicture.asset(
-                                    'assets/arrowForword.svg'),
-                                onPressed: () {})
-                          ],
-                        ),
-                      ],
-                    ),
-                  )).toList()
-                ),
-              );
-            }
+              return CustomCarousel(offers: offers,);
+            }),
 
-        )
+        ProductView()
+
       ]),
     );
   }
+
+  void initController() {}
 }
